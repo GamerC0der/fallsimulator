6da2996c8a936f850390b02de59f2b0bca358ba9
GamerC0der <159653318+GamerC0der@users.noreply.github.com>
2025-10-24 19:40:44 -0700

    New upgraded maple syrup upgrade

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def serve_static(filename):
     return send_from_directory('.', filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
